2ceac106635b8be57a4cd9f92d9ff8053ae94209
Bobby Radford <bobby@rownd.io>
2025-07-29 21:01:54 -0500

    chore: add basic Dockerfiles

---
+++ designer/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -euo pipefail
+
+# Capture all environment variables starting with VITE_APP_ and make JSON string from them
+ENV_JSON="$(jq --compact-output --null-input 'env | with_entries(select(.key | startswith("VITE_APP_")))')"
+# Escape sed replacement's special characters: \, &, /.
+# No need to escape newlines, because --compact-output already removed them.
+# Inside of JSON strings newlines are already escaped.
+ENV_JSON_ESCAPED="$(printf "%s" "${ENV_JSON}" | sed -e 's/[\&/]/\\&/g')"
+
+sed -i "s/<noscript id=\"env-insertion-point\"><\/noscript>/<script>var ENV=${ENV_JSON_ESCAPED}<\/script>/g" ./index.html
+
+exec "$@"
