54727120fdff6a2dfa410e50989c84e50eb61a63
Davon Davis <davond@Davons-MBP.attlocal.net>
2025-08-05 12:42:09 -0400

    fix scrolling

--- designer/src/Chat.tsx
+++ designer/src/Chat.tsx
@@ -6,14 +6,14 @@ function Chat() {
   const [isPanelOpen, setIsPanelOpen] = useState<boolean>(true)
 
   return (
-    <div className="w-full h-full flex pt-12 transition-colors bg-gray-50 dark:bg-blue-800">
+    <div className="w-full h-full flex transition-colors bg-gray-50 dark:bg-blue-800 pt-12">
       <div
         className={`h-full transition-all duration-300 ${isPanelOpen ? 'w-1/4' : 'w-[47px]'}`}
       >
         <Chatbox isPanelOpen={isPanelOpen} setIsPanelOpen={setIsPanelOpen} />
       </div>
       <div
-        className={`h-full ${isPanelOpen ? 'w-3/4' : 'flex-1'} text-gray-900 dark:text-white`}
+        className={`h-full scrollbar-thin overflow-auto ${isPanelOpen ? 'w-3/4' : 'flex-1'} text-gray-900 dark:text-white pb-6`}
       >
         <Outlet />
       </div>
