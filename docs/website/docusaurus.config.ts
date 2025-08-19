568cbd256d023968638a7d76827e8a7e58e5a1f0
Matt Hamann <mhamann@rownd.io>
2025-08-19 13:44:37 -0400

    fix(docs): include gh actions in deploy package (#80)
    * fix(docs): include gh actions in deploy package
    
    * chore: test deploy updates
    
    * fix(docs): use docs build artifact for deploy
    
    * fix(docs): use nx from npm
    
    * fix(docs): pre install nx
    
    * fix(docs): run nx init
    
    * fix(docs): nx install
    
    * fix(docs): nx init
    
    * fix(docs): nx working dir
    
    * fix(docs): nx working dir
    
    * fix(docs): change deploy branch
    
    * fix(docs): use gh-pages for deploys
    
    * fix(docs): use gh-pages for deploys
    
    * chore: finishing touches
    
    * chore: cleanup

--- docs/website/docusaurus.config.ts
+++ docs/website/docusaurus.config.ts
@@ -25,7 +25,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'llama-farm', // Usually your GitHub org/user name.
   projectName: 'llama-farm.github.io', // Usually your repo name.
-  deploymentBranch: 'main',
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
