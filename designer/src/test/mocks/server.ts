14aeb4f55132c7a0951b35c75f61a788822cbe74
rgthelen <rgthelen@gmail.com>
2026-01-03 19:08:49 -0600

    chore: remove designer, docs, and electron-app for smaller version

--- designer/src/test/mocks/server.ts
+++
@@ -1,9 +0,0 @@
-import { setupServer } from 'msw/node'
-import { handlers } from './handlers'
-
-/**
- * MSW Server for Node environment (used in tests)
- * This server intercepts HTTP requests during tests and returns mock responses
- */
-export const server = setupServer(...handlers)
-
